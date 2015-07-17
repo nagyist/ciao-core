252bc8c95d09ea9c4f40ef6531d782c9bc52a6a3
Iestyn Evans <Iestyn.Evans@bjss.com>
2015-07-17 12:34:41 +0100

    Extracted TreeMerge util class from ciao-docs-enricher module

---
+++ src/main/java/uk/nhs/ciao/util/TreeMerge.java
@@ -0,0 +1,54 @@
+package uk.nhs.ciao.util;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+/**
+ * Merges nodes from one tree structure into another
+ */
+public class TreeMerge {
+	// Perhaps some configuration options could be added (i.e. whether to merge lists, overwrite defaults etc)
+	
+	public void mergeInto(final Map<String, ?> source, final Map<String, Object> destination) {
+		for (final Entry<String, ?> entry: source.entrySet()) {
+			final Object destinationValue = destination.get(entry.getKey());
+			final Object mergedValue = mergeValue(entry.getValue(), destinationValue);
+			if (mergedValue != destinationValue) {
+				destination.put(entry.getKey(), mergedValue);
+			}
+		}
+	}
+	
+	private Object mergeValue(final Object sourceValue, final Object destinationValue) {
+		if (destinationValue == null) {
+			return sourceValue;
+		} else if (destinationValue instanceof Map) {
+			@SuppressWarnings("unchecked")
+			final Map<String, Object> toMap = (Map<String, Object>)destinationValue;
+			mergeValueIntoMap(sourceValue, toMap);
+		} else if (destinationValue instanceof List) {
+			@SuppressWarnings("unchecked")
+			final List<Object> toList = (List<Object>)destinationValue;
+			mergeValueIntoList(sourceValue, toList);
+		} // else - do not overwrite!
+		
+		return destinationValue;
+	}
+	
+	private void mergeValueIntoMap(final Object value, final Map<String, Object> destination) {
+		if (value instanceof Map) {
+			@SuppressWarnings("unchecked")
+			final Map<String, ?> fromMap = (Map<String, ?>)value;
+			mergeInto(fromMap, destination);
+		} // else for not overwrite!
+	}
+	
+	private void mergeValueIntoList(final Object value, final List<Object> destination) {
+		if (value instanceof List) {
+			destination.addAll((List<?>)value);
+		} else {
+			destination.add(value);
+		}
+	}
+}
