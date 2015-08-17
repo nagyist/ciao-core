dc4d0ba2a0c05df026cc861b635399589177aab6
Iestyn Evans <Iestyn.Evans@bjss.com>
2015-08-17 11:21:26 +0100

    Updated Whitespace util method names

--- src/main/java/uk/nhs/ciao/util/Whitespace.java
+++ src/main/java/uk/nhs/ciao/util/Whitespace.java
@@ -5,15 +5,31 @@ import java.util.regex.Pattern;
 public class Whitespace {
 	private static final Pattern MULTIPLE_WHITESPACE = Pattern.compile("\\s+");
 	
-	public static String collapseWhitespace(final String text) {
-		return collapseWhitespace(text, " ");
+	public static String collapseWhitespaceAndTrim(final CharSequence text) {
+		return collapseWhitespaceAndTrim(text, " ");
 	}
 	
-	public static String collapseWhitespace(final String text, final String replacement) {
-		if (text == null || text.isEmpty()) {
-			return text;
+	public static String collapseWhitespaceAndTrim(final CharSequence text, final String replacement) {
+		if (text == null) {
+			return null;
+		} else if (text.length() == 0){
+			return text.toString();
 		}
 		
-		return MULTIPLE_WHITESPACE.matcher(text).replaceAll(replacement).trim();
+		return collapseWhitespace(text, replacement).trim();
+	}
+	
+	public static String collapseWhitespace(final CharSequence text) {
+		return collapseWhitespace(text);
+	}
+	
+	public static String collapseWhitespace(final CharSequence text, final String replacement) {
+		if (text == null) {
+			return null;
+		} else if (text.length() == 0){
+			return text.toString();
+		}
+		
+		return MULTIPLE_WHITESPACE.matcher(text).replaceAll(replacement);
 	}
 }
